af0289ab0a5f229ab30bb62500d73a5a4ce6ca38
Cristian Angelo Manica Georgiev <cmanica-@car10s2.42barcelona.com>
2026-02-03 04:00:13 +0100

    hecho strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,3 +1,24 @@
 section .text
 global ft_strcpy
 
+ft_strcpy: ; rdi is dest, rsi is src
+	push rbp
+	mov rbp, rsp
+	mov rax, 0
+	jmp .loop
+
+	
+.loop:
+	cmp byte [rsi + rax], 0
+	je .end
+	mov cl, byte [rsi + rax]
+	mov byte [rdi + rax], cl
+	inc rax
+	jmp .loop
+	
+.end:
+	mov cl, byte [rsi + rax]
+	mov byte [rdi + rax], cl
+	mov rax, rdi
+	leave
+	ret
